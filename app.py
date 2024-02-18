7cad4d64bd5cfbc72c8ffde3b202dd8c5846ead9
Renato Almeida <renatoam@ymail.com>
2024-02-18 12:06:34 -0300

    feat: create update meal route

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def test():
 
 @app.route("/meals",methods=['POST'])
 def create_meal():
-    data = request.json;
+    data = request.json
     name = data.get("nome")
     description = data.get("descricao")
     date_time = data.get("data_hora")
@@ -29,6 +29,33 @@ def create_meal():
             db.session.add(meal)
             db.session.commit()
             return jsonify({"message": "Refeição cadastrada com sucesso"}), 200
+    return jsonify({"message":"Dados invalidos"}), 401
+
+@app.route("/meals/<int:id_meal>",methods=['PUT'])
+def update_meal(id_meal):
+    data = request.json
+    name = data.get("nome")
+    description = data.get("descricao")
+    date_time = data.get("data_hora")
+    inside_diet = data.get("dentro_da_dieta")
+    user_id = int(data.get("usuario"))
+    if id_meal :
+        meal = Meal.query.get(id_meal) 
+        user = User.query.get(user_id)
+        if user is None :
+            return jsonify({"message": f"Usuário {user_id} não encontrado" })
+        if meal is None: 
+            return jsonify({"message": f"Refeição {id_meal} não encontrada" })
+        if  name and description and date_time:
+            meal.name = name
+            meal.description = description
+            meal.date_time = date_time
+            meal.inside_diet = inside_diet
+            meal.user_id = user_id 
+            db.session.commit()
+            return jsonify({"message":f"Refeição {id_meal} atualizada com sucesso "}), 200
+        return jsonify({"message": "Dados incompletos"}), 404
+    
     return jsonify({"message":"Dados invalidos"}), 401
 
 
