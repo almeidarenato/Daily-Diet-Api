250143068ece467aa6fa682e2e6fca17b2019457
Renato Almeida <renatoam@ymail.com>
2024-02-17 23:23:29 -0300

    feat: add meal register route

--- app.py
+++ app.py
@@ -13,5 +13,24 @@ db.init_app(app)
 def test():
     return jsonify({"message": "Testing my app"}),200
 
+@app.route("/meals",methods=['POST'])
+def create_meal():
+    data = request.json;
+    name = data.get("nome")
+    description = data.get("descricao")
+    date_time = data.get("data_hora")
+    inside_diet = data.get("dentro_da_dieta")
+    user_id = int(data.get("usuario"))
+
+    if name and description and date_time and inside_diet and user_id:
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            meal = Meal(name=name,description=description,date_time=date_time,inside_diet=inside_diet,user_id=user.id)
+            db.session.add(meal)
+            db.session.commit()
+            return jsonify({"message": "Refeição cadastrada com sucesso"}), 200
+    return jsonify({"message":"Dados invalidos"}), 401
+
+
 if __name__ == '__main__':
     app.run(debug=True)
